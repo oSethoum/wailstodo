bf55be868e1776fb779f48071d45f60ea2ed3a82
Oussama Sethoum <os.sethoum@gmail.com>
2022-11-08 23:00:43 +0100

    made some changes

--- app.go
+++ app.go
@@ -12,12 +12,12 @@ type App struct {
 
 // NewApp creates a new App application struct
 func NewApp() *App {
+	go backend.Serve()
 	return &App{}
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
-	go backend.Serve()
 	a.ctx = ctx
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed all:frontend/dist
@@ -24,11 +22,7 @@ func main() {
 		Assets:           assets,
 		BackgroundColour: &options.RGBA{R: 26, G: 27, B: 30},
 		OnStartup:        app.startup,
-		StartHidden:      true,
 		MinWidth:         420,
-		OnDomReady: func(ctx context.Context) {
-			runtime.WindowShow(ctx)
-		},
 		Bind: []interface{}{
 			app,
 		},
